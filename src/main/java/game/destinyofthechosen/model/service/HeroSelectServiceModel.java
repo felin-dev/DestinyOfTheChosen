427afe406ead483bb91021e76a093ec7b02f6fe0
felin-dev <simeon.stanislavov.balabanov@gmail.com>
2021-11-13 20:15:03 +0200

    added 500 status code error page;

---
+++ src/main/java/game/destinyofthechosen/model/service/HeroSelectServiceModel.java
@@ -0,0 +1,17 @@
+package game.destinyofthechosen.model.service;
+
+import java.util.UUID;
+
+public class HeroSelectServiceModel {
+
+    private UUID id;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public HeroSelectServiceModel setId(UUID id) {
+        this.id = id;
+        return this;
+    }
+}
